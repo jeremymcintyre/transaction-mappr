0b83cd08b6d5518dbecd336e69760f93f9de5468
jeremymcintyre <jmcintyre.dev@gmail.com>
2015-03-25 16:42:23 -0700

    In maps.js, add myApp object to manage globals; refactor bindClickEvents to be reusable; capture map variable in scope of setMarkers method and store in myApp; implement proof-of-concept AJAX call that gets JSON from the backend and generates markers with the response

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,4 +1,7 @@
+var myApp = {};
+
 $(document).ready(function() {
+
   function initialize() {
     var mapOptions = {
       center: new google.maps.LatLng(37.773972, -122.431297),
@@ -6,16 +9,39 @@ $(document).ready(function() {
     };
     var map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
+    window.map = map;
   }
   google.maps.event.addDomListener(window, 'load', initialize);
 
-  bindEvents();
+
+  // event binding for reuse
+  function bindClickEvents(element, callback) {
+  $(element).on('click', callback);
+  }
+  // setMode is for determining what to request in Ajax calls
+  bindClickEvents('nav a', setMode);
+
+
+  // capture 'map' in scope for use in making markers
+  myApp.setMarkers = function(locations) {
+    console.log("available");
+    var loadMap = map;
+    for (var i = 0, len = locations.length; i < len; i++) {
+      var location = locations[i],
+          position = new google.maps.LatLng(location.latitude, location.longitude);
+
+      console.log(location);
+      var marker = new google.maps.Marker({
+        position: position,
+        map: map
+      });
+    }
+  };
 });
 
 
-function bindEvents() {
-  $('nav a').on('click', setMode);
-}
+
+
 
 function setMode(event) {
   event.preventDefault();
@@ -24,11 +50,13 @@ function setMode(event) {
   window.mode = mode;
   if (mode === "all") {
     $.ajax({
-      // create route to get all
-      url: '/all',
-      type: 'GET'
+      type: 'GET',
+      url: '/all'
     }).success(function(resp) {
-      console.log(resp);
+      // currently limiting data to first user for proof of concept
+      var currentLocations = resp.locations['Art Treaster'];
+      var currentTransactions = resp.transactions['Art Treaster'];
+      myApp.setMarkers(current);
     });
   }
 }
