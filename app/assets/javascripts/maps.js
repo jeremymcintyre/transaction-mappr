c2a8b7c91c2fdb45830cb8fbcfe68ce24f007fcc
jeremymcintyre <jmcintyre.dev@gmail.com>
2015-03-25 17:42:39 -0700

    Refactor myApp in maps.js to have a MarkersCtrl

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,63 +1,79 @@
-var myApp = {};
+var myApp = {
+
+  MarkersCtrl: (function(){
+    var markers = [];
+
+    return {
+
+      setMarkers: function(locations) {
+        var map = myApp.map;
+        for (var i = 0, len = locations.length; i < len; i++) {
+          var location = locations[i],
+              position = new google.maps.LatLng(location.latitude,
+                location.longitude),
+
+              marker = new google.maps.Marker({
+                position: position,
+                animation: google.maps.Animation.DROP,
+                map: map
+              });
+
+          markers.push(marker);
+        }
+      },
+
+      clearMarkers: function() {
+        for (i=0; i < markers.length; i++) {
+          markers[i].setMap(null);
+        }
+        // remove references to markers
+        markers.length = 0;
+      }
+
+    };
+  })()
+
+};
+
 
 $(document).ready(function() {
-
   function initialize() {
     var mapOptions = {
       center: new google.maps.LatLng(37.773972, -122.431297),
       zoom: 12
     };
-    var map = new google.maps.Map(document.getElementById('map-canvas'),
+    // capture 'map' in scope for use in making markers
+    myApp.map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
-    window.map = map;
   }
   google.maps.event.addDomListener(window, 'load', initialize);
 
 
   // event binding for reuse
   function bindClickEvents(element, callback) {
-  $(element).on('click', callback);
+    $(element).on('click', callback);
   }
   // setMode is for determining what to request in Ajax calls
   bindClickEvents('nav a', setMode);
 
+  function setMode(event) {
+    event.preventDefault();
 
-  // capture 'map' in scope for use in making markers
-  myApp.setMarkers = function(locations) {
-    console.log("available");
-    var loadMap = map;
-    for (var i = 0, len = locations.length; i < len; i++) {
-      var location = locations[i],
-          position = new google.maps.LatLng(location.latitude, location.longitude);
-
-      console.log(location);
-      var marker = new google.maps.Marker({
-        position: position,
-        map: map
-      });
-    }
-  };
-});
-
-
-
-
-
-function setMode(event) {
-  event.preventDefault();
-
-  var mode = this.innerHTML.toLowerCase();
-  window.mode = mode;
-  if (mode === "all") {
-    $.ajax({
-      type: 'GET',
-      url: '/all'
-    }).success(function(resp) {
+    var mode = this.innerHTML.toLowerCase();
+    myApp.mode = mode;
+    if (mode === "all") {
+      $.ajax({
+        type: 'GET',
+        url: '/all'
+      }).success(function(resp) {
       // currently limiting data to first user for proof of concept
       var currentLocations = resp.locations['Art Treaster'];
       var currentTransactions = resp.transactions['Art Treaster'];
-      myApp.setMarkers(current);
+      console.log(currentTransactions);
+      myApp.MarkersCtrl.setMarkers(currentLocations);
     });
+    }
   }
-}
+});
+
 
