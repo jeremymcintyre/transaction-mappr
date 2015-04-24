78a7281f88399ee6259a837f2df7b647a33affbf
jeremymcintyre <jmcintyre.dev@gmail.com>
2015-04-23 23:16:19 -0700

    Remove custom csv rake tasks in favor of putting the code in the seeds.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'csv'
+
+csv_file_path = 'db/users.csv'
+
+CSV.foreach(csv_file_path, headers: true) do |row|
+  User.create!({
+    name: row[1]
+    })
+end
+
+csv_file_path = 'db/transactions.csv'
+
+CSV.foreach(csv_file_path, headers: true) do |row|
+  Transaction.create!({
+    created_at: row[1],
+    user_id: row[2],
+    transaction_type: row[3],
+    amount: row[4]
+    })
+end
+
+csv_file_path = 'db/locations.csv'
+
+CSV.foreach(csv_file_path, headers: true) do |row|
+  Location.create!({
+    created_at: row[1],
+    user_id: row[2],
+    longitude: row[3],
+    latitude: row[4]
+    })
+end
