7d02375db36e8e97a80e6e0bdab8c0a8c25b787d
jeremymcintyre <jmcintyre.dev@gmail.com>
2015-04-24 15:53:27 -0700

    Make UserController lighter by abstracting all computations into user_helper.rb

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,62 +1,16 @@
 class UserController < ApplicationController
-  def all_users
-    User.all
-  end
+  include UserHelper
 
   def all
-    users_locations = {}
-    users_transactions = {}
-    all_users.each do |user|
-      users_locations[[user.id, user.name]] = user.locations
-      users_transactions[[user.id, user.name]] = user.transactions
-    end
-    render :json => {locations: users_locations, transactions: users_transactions}
+    get_all_users_transactions_and_users_locations
+    render :json => {locations: @users_locations, transactions: @users_transactions}
   end
 
   def get_transactions_and_locations_on_date
-    date, type = params[:date], params[:filter]
-    transactions = get_transactions_on_date(date, type)
+    date, filter = params[:date], params[:filter]
+    transactions = get_transactions_on_date(date, filter)
     locations = get_locations_on_date(date)
     render :json => {transactions: transactions, locations: locations}
   end
-
-  private
-
-    def get_transactions_on_date(date, type)
-      transactions = {}
-      date = parse_date(date)
-      query = "created_at = ?"
-
-      if type == "both"
-        all_users.each do |user|
-          user_transactions =
-            user.transactions.where(query, date)
-          transactions[[user.id, user.name]] = user_transactions unless user_transactions.empty?
-        end
-      else
-        query += " AND transaction_type = ?"
-        all_users.each do |user|
-          user_transactions =
-            user.transactions.where(query, date, type)
-          transactions[[user.id, user.name]] = user_transactions unless user_transactions.empty?
-        end
-      end
-      transactions
-    end
-
-    def get_locations_on_date(date)
-      locations = {}
-      date = parse_date(date)
-
-      all_users.each do |user|
-        user_locales = user.locations.where("created_at = ?", date)
-        locations[[user.id, user.name]] = user_locales unless user_locales.empty?
-      end
-      locations
-    end
-
-    def parse_date(date)
-      Time.parse(date).utc.beginning_of_day
-    end
 
 end
--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,2 +1,52 @@
 module UserHelper
+
+  def all_users
+    User.all
+  end
+
+  def get_all_users_transactions_and_users_locations
+    @users_locations = {}
+    @users_transactions = {}
+
+    all_users.each do |user|
+      @users_locations[[user.id, user.name]] = user.locations
+      @users_transactions[[user.id, user.name]] = user.transactions
+    end
+  end
+
+  def get_transactions_on_date(date, filter)
+    transactions = {}
+    date = parse_date(date)
+    query = "created_at = ?"
+
+    if filter == "both"
+      all_users.each do |user|
+        user_transactions = user.transactions.where(query, date)
+        transactions[[user.id, user.name]] = user_transactions unless user_transactions.empty?
+      end
+    else
+      query += " AND transaction_type = ?"
+      all_users.each do |user|
+        user_transactions = user.transactions.where(query, date, filter)
+        transactions[[user.id, user.name]] = user_transactions unless user_transactions.empty?
+      end
+    end
+    transactions
+  end
+
+  def get_locations_on_date(date)
+    locations = {}
+    date = parse_date(date)
+
+    all_users.each do |user|
+      user_locales = user.locations.where("created_at = ?", date)
+      locations[[user.id, user.name]] = user_locales unless user_locales.empty?
+    end
+    locations
+  end
+
+  def parse_date(date)
+    Time.parse(date).utc.beginning_of_day
+  end
+
 end
